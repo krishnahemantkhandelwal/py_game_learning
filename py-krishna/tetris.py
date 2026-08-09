c20de81357835a1f26e7c9fa4509c0368d52f3bc
krishnahemantkhandelwal <krishnahemantkhandelwal@gmail.com>
2026-08-09 22:40:37 +0530

    edited

--- py-krishna/tetris.py
+++ py-krishna/tetris.py
@@ -14,22 +14,27 @@ grid = [
 colors = [(255,0,0), (0,255,0), (0,0,255), (255,255,0), (255,0,255), (0, 255, 255), (255, 127, 0)]
 def xy_calc(gridx, gridy, width=30, height=30):
     return (((gridx-1)*30), ((gridy-1)*30), width, height)
-y = 1
-current_piece = [[5, y, random.choice(colors), 1]] #x, y, color, speed
-grid[y][5] = [5, y, random.choice(colors)]
-#--------------------------------------------------------------------#
+#y = 1
+current_piece = [[5, 1, random.choice(colors), 1]] #x, y, color, speed
+#piece_falling = True        
+#-------------------------------------------------------------------------------------------#
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+#---------------------------------------------------------------------#
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, grid[y][5][2], xy_calc(5, y))
-    if y!=20:
-        if grid[y+1][5] == []:
-            temp = current_piece[0][:] 
-            grid[y][5].clear()
-            y+=1
-            grid[y][5] = temp #huedhei
+    if current_piece != []:
+        pygame.draw.rect(screen, current_piece[0][2], xy_calc(current_piece[0][0], current_piece[0][1]))
+    for i in range(21):
+        for j in range(11):
+            if grid[i][j] != []:
+                pygame.draw.rect(screen, grid[i][j], xy_calc(j,i))
+    if current_piece != [] and current_piece[0][1]!=len(grid)-1 and grid[current_piece[0][1]+1][current_piece[0][0]] == []:
+        current_piece[0][1] += current_piece[0][3]
+    elif current_piece != []:
+        grid[current_piece[0][1]][current_piece[0][0]].append(current_piece[0][2])
+        current_piece.clear()
     pygame.display.flip()
     clock.tick(10)
     
