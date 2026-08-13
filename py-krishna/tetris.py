560fe64069c7463dadab7558bb5228daa2052c0c
krishnahemantkhandelwal <krishnahemantkhandelwal@gmail.com>
2026-08-13 16:50:57 +0530

    hi

--- py-krishna/tetris.py
+++ py-krishna/tetris.py
@@ -17,58 +17,59 @@ def xy_calc(gridx, gridy, width=30, height=30):
     return (((gridx-1)*30), ((gridy-1)*30), width, height)
 def add_piece(xy:list, speed=1):
     return [xy, random.choice(colors), speed]
-current_piece = [[[5, 1], [6, 1], [5, 2], [6, 2]], random.choice(colors), 1] #x, y, color, speed
+
 shape = {
     "O": {
         1: np.array([0, 0]),
         2: np.array([1, 0]),
         3: np.array([0, 1]),
         4: np.array([1, 1]),
-        "bottum blocks": np.array([3, 4])
+        
     },
     "I": {
         1: np.array([0, 0]),
         2: np.array([1, 0]),
         3: np.array([2, 0]),
         4: np.array([-1, 0]),
-        "bottum blocks": np.array([1, 2, 3, 4])
+        
     },
     "T": {
         1: np.array([0, 0]),
         2: np.array([1, 0]),
         3: np.array([-1, 0]),
         4: np.array([0, 1]),
-        "bottum blocks": np.array([2, 3, 4])
+        
     },
     "L": {
         1: np.array([0, 0]),
-        2: np.array([0, 0]),
+        2: np.array([0, 1]),
         3: np.array([0, 2]),
         4: np.array([1, 2]),
-        "bottum blocks": np.array([3, 4])
+        
     },
     "J": {
         1: np.array([0, 0]),
         2: np.array([0, 1]),
         3: np.array([0, 2]),
         4: np.array([-1, 2]),
-        "bottum blocks": np.array([3, 4])
+        
     },
     "S": {
         1: np.array([0, 0]),
         2: np.array([1, 0]),
         3: np.array([0, 1]),
         4: np.array([-1, 1]),
-        "bottum blocks": np.array([2, 3, 4])
+        
     },
     "Z": {
         1: np.array([0, 0]),
         2: np.array([-1, 0]),
         3: np.array([0, 1]),
         4: np.array([1, 1]),
-        "bottum blocks": np.array([2, 3, 4])
     }
 }
+rand_piece = random.choice(["O", "I", "T", "S", "Z", "J", "L"])
+current_piece = [[shape[rand_piece][1]+[5, 1], shape[rand_piece][2]+[5, 1], shape[rand_piece][3]+[5, 1], shape[rand_piece][4]+[5, 1]], random.choice(colors), 1] #x, y, color, speed
 def rotate(x, y):
     return [y, -x]
 #-------------------------------------------------------------------------------------------#
@@ -81,7 +82,8 @@ while running:
 #---------------------------------------------------------------------#
     screen.fill((0, 0, 0))
     if current_piece == []:
-        current_piece = add_piece([[5, 1], [6, 1], [5, 2], [6, 2]])
+        rand_piece = random.choice(["O", "I", "T", "S", "Z", "J", "L"])
+        current_piece = add_piece([shape[rand_piece][1]+[5, 1], shape[rand_piece][2]+[5, 1], shape[rand_piece][3]+[5, 1], shape[rand_piece][4]+[5, 1]])
     if current_piece != []:
         for i in current_piece[0][:]:
             pygame.draw.rect(screen, current_piece[1], xy_calc(i[0], i[1]))
